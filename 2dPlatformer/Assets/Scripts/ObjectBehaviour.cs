09813a88fa7241917c8658ef82b6fabb075a85cc
Monika Shaw <monika.shaw@NCIRCLETECH.COM>
2025-10-29 13:39:00 +0530

    Developed a 2D Game 2D-Platformer

---
+++ 2dPlatformer/Assets/Scripts/ObjectBehaviour.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectBehaviour : MonoBehaviour
+{
+   [SerializeField] GameObject prefab;
+    bool _gameOver = false;
+
+   public void SpawnObject()
+   {
+    Instantiate(prefab, new Vector3(Random.Range(-8f,8f), 6f, 0f), Quaternion.identity);
+   }
+
+private void OnCollisionEnter2D(Collision2D collision)
+{
+    if(collision.gameObject.tag =="Player" && !_gameOver)
+    {
+        SpawnObject();
+        Destroy(gameObject);
+    }
+    else if(collision.gameObject.tag =="Ground")
+    {
+        _gameOver = true;
+        Debug.Log("GAME OVER! YOU LOOSE!");
+    }
+}
+
+}
---
+++ 2dPlatformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D _rb;
+
+    [SerializeField] ObjectBehaviour _objectBehaviour;
+
+    float _playerSpeed = 10f;
+    float _inputHorizontal;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _rb = gameObject.GetComponent<Rigidbody2D>();
+        _objectBehaviour.SpawnObject();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _inputHorizontal = Input.GetAxisRaw("Horizontal");
+
+        if(_inputHorizontal != 0){
+
+            _rb.AddForce(new Vector2(_inputHorizontal * _playerSpeed, 0f));
+        }
+        
+    }
+}
