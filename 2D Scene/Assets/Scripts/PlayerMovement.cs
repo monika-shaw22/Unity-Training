14e6553efd7afe18e787a8c0a799b0c005e3eebe
Monika Shaw <monika.shaw@NCIRCLETECH.COM>
2025-10-29 11:47:55 +0530

    Simple 2D Movement

---
+++ 2D Scene/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    public float moveSpeed = 5f;
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveX, moveY, 0f);
+        transform.Translate(movement);
+    }
+}
