2f3cd2bd34ec7ed4252d3b1f0599e2f986f48608
Monika Shaw <monika.shaw@NCIRCLETECH.COM>
2025-10-29 17:15:25 +0530

    FPS Camera Learning

---
+++ Day 3/Assets/Scripts/FirstPersonCamera.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // Prevent flipping
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); // Vertical look
+        playerBody.Rotate(Vector3.up * mouseX); // Horizontal look
+    }
+}
